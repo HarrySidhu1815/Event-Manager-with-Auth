ec860b6cd1a6390e75ea94eeab6e0587b6640a61
Harjobanpreet Sidhu <harjobanpreetsingh.sidhu@student.ufv.ca>
2024-07-12 16:58:07 -0700

    Initial commit

---
+++ frontend/src/pages/Authentication.js
@@ -0,0 +1,49 @@
+import { json, redirect } from 'react-router-dom';
+import AuthForm from '../components/AuthForm';
+
+function AuthenticationPage() {
+  return <AuthForm />;
+}
+
+export async function action({request}){
+  const data = await request.formData()
+  const searchParam = new URL(request.url).searchParams
+
+  const authData = {
+    email: data.get('email'),
+    password: data.get('password')
+  } 
+  
+  const mode = searchParam.get('mode') || 'login'
+
+  if(mode !== 'login' && mode !== 'signup'){
+    throw json({message: 'Unsupported mode!'}, {status: 422})
+  }
+  
+  const response = await fetch('http://localhost:8080/' + mode, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(authData)
+  })
+
+  if(response.status === 422 || response.status === 401){
+    return response
+  }
+
+  if(!response.ok){
+    return json({message: 'Could not fetch the user!'}, {status: 500})
+  } 
+    const resData = await response.json()
+    const token = resData.token
+
+    localStorage.setItem('token', token)
+    const expiration = new Date()
+    expiration.setHours(expiration.getHours() + 1)
+    localStorage.setItem('expiration', expiration.toISOString)
+    return redirect('/')
+  
+}
+
+export default AuthenticationPage;
